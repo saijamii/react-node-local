94f7b32d65f0dc0453a34c457001aa38706aec1e
saijamii <saijami38@gmail.com>
2024-03-09 15:17:57 +0530

    User Details Terminal changes

--- React/src/routes/UserDetails.js
+++ React/src/routes/UserDetails.js
@@ -17,6 +17,7 @@ export default function UserDetails(props) {
       .catch((error) => {
         console.log(error);
       });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
